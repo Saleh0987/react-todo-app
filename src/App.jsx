25a2d32629dfc6c24ed76fa0da4288284620abe9
Saleh0987 <ms3617224@gmail.com>
2023-02-08 00:35:57 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const todosItem = useSelector((state) => state.todos.todosList);
 
   return (
-    <div className='w-full min-h-screen pt-4 font-bodyFont bg-gradient-to-t from-sky-600 via-sky-400 to-sky-300 text-white px-4 flex flex-col gap-10 justify-center items-center'>
+    <div className='w-full min-h-screen pt-4 font-bodyFont bg-gradient-to-t from-sky-700 via-sky-500 to-sky-300 text-white px-4 flex flex-col gap-10 justify-center items-center'>
       {
         todosItem.length > 0 ? <Category /> : ''
       }
